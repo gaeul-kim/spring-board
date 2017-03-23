515abef0474eb03c58b5d2f860e1f09228628b18
sangsik <toheart@gmail.com>
2017-03-23 17:38:11 +0900

    테스트 도메인 삭제

--- src/test/java/sampleProject/test/controller/TestController.java
+++ src/test/java/sampleProject/test/controller/TestController.java
@@ -6,16 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import sampleProject.test.domain.Member;
-
 @Controller
 public class TestController {
     Logger LOG = Logger.getLogger(this.getClass());
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String test(Model model) {
-        model.addAttribute("member", new Member());
-
         return "/user/test";
     }
 }
